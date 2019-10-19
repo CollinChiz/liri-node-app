084fd0608fc5ebeb0320397f0ca4b2531e2af3ef
chizzy <cwchiz16@gmail.com>
2019-10-19 12:48:00 -0600

    inital setup

---
+++ liri.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+var concertThis = function() {
+    
+}
+ var command = process.argv[2]
+ 
+ switch(command) {
+    case "concert-this": 
+    //  code
+    break;
+    
+    case "spotify-this-song":
+        // code
+        break;
+    
+    case "movie-this":
+        // code
+        break;
+
+    case "do-what-it-says":
+        // code
+        break;
+ }
+ 
