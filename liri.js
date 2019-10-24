628b36ca5d93f902a20a5c8b4dfcff81d5bfcb15
chizzy <cwchiz16@gmail.com>
2019-10-23 20:15:24 -0600

    concert-this function

--- liri.js
+++ liri.js
@@ -1,19 +1,36 @@
+// input band
+// use band apply to api
+// return name of venue, location, time
+var axios = require("axios");
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 var concertThis = function() {
-    
+     
 }
- var command = process.argv[2]
- 
- switch(command) {
+
+var command = process.argv[2]
+
+switch(command) {
     case "concert-this": 
     //  code
+    var artist = process.argv[3]
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function(response) {
+            for(i=0; i<response.data.length; i++) {
+                console.log(response.data[i].venue.name);
+                console.log(response.data[i].venue.city);
+                console.log(response.data[i].datetime).format("MM/DD/YY");
+            }
+        }
+    );
+    
     break;
     
     case "spotify-this-song":
         // code
+        
         break;
     
     case "movie-this":
